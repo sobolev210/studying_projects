ce28b714fe3364e184c89def9dbde7765c676d65
sobolev210 <68656754+sobolev210@users.noreply.github.com>
2020-11-03 20:06:39 +0300

    Update main.py

--- pythonProject13/main.py
+++ pythonProject13/main.py
@@ -1,11 +1,11 @@
 import cv2
-smile_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_smile.xml")
+face = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 
 
-img = cv2.imread("Nastja.png")
+img = cv2.imread("bclub.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = smile_cascade_db.detectMultiScale(img_gray, 1.05, 100)
+faces = face.detectMultiScale(img_gray, 1.05, 100)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
